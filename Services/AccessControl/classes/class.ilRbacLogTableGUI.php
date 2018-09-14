254b10aa24e824839bf1be18afd79d7665102290
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2018-09-14 10:27:23 +0200

    1776 get translation in log from plugin

--- Services/AccessControl/classes/class.ilRbacLogTableGUI.php
+++ Services/AccessControl/classes/class.ilRbacLogTableGUI.php
@@ -196,6 +196,7 @@ class ilRbacLogTableGUI extends ilTable2GUI
 		return $result;
 	}
 	
+	// cat-tms-patch start
 	// #10946
 	protected function getOPCaption($a_type, $a_op)
 	{
@@ -205,6 +206,7 @@ class ilRbacLogTableGUI extends ilTable2GUI
 			$res = array();
 			foreach($a_op as $op)
 			{
+				$dd = substr($op, strpos($op, "_"));
 				$res[] = $this->getOPCaption($a_type, $op);
 			}
 			return implode(", ", $res);
@@ -214,15 +216,65 @@ class ilRbacLogTableGUI extends ilTable2GUI
 		{
 			$op_id = $this->operations[$a_op];
 			if(substr($op_id, 0, 7) != "create_")
-			{				
-				return $this->lng->txt($a_type."_".$this->operations[$a_op]);
+			{
+				$perm = $this->getTranslationFromPlugin($a_type, $op_id);
+
+				if($this->notTranslated($perm, $op_id)) {
+					if($this->lng->exists($a_type.'_'.$op_id.'_short'))
+					{
+						$perm = $this->lng->txt($a_type.'_'.$op_id.'_short');
+					}
+					else
+					{
+						$perm = $this->lng->txt($op_id);
+					}
+				}
+
+				return $perm;
 			}
 			else
 			{
-				return $this->lng->txt("rbac_".$this->operations[$a_op]);
+				$type = substr($op_id, 7, strlen($op_id));
+				$perm = $this->getTranslationFromPlugin($type, $op_id);
+
+				if($this->notTranslated($perm, $op_id)) {
+					$perm = $this->lng->txt("rbac_".$op_id);
+				}
+
+				return $perm;
 			}
 		}
 	}
+
+	/**
+	 * Check the type for plugin and get the translation for op_id
+	 *
+	 * @param string 	$type
+	 * @param string 	$op_id
+	 * @return string | null
+	 */
+	protected function getTranslationFromPlugin($type, $op_id)
+	{
+		global $objDefinition;
+
+		if($objDefinition->isPlugin($type)) {
+			return ilObjectPlugin::lookupTxtById($type, $op_id);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Check the op is translated correctly
+	 *
+	 * @param string 	$type
+	 * @param string 	$op_id
+	 * @return bool
+	 */
+	protected function notTranslated($perm, $op_id) {
+		return is_null($perm) || (strpos($perm, $op_id) !== false);
+	}
+	// cat-tms-patch end
 }
 
 ?>
